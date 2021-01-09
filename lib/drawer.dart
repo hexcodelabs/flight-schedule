0b3c7df41388aba92a64a4eab41b69152cea8b46
UdithaIshan <uditha.ishan34@gmail.com>
2021-01-09 11:43:50 +0530

    Drawer column replaced with listView

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -7,36 +7,43 @@ class DrawerWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.horizontal(right: Radius.circular(30.0)),
-      child: Drawer(
-        child: Container(
-          padding: EdgeInsets.fromLTRB(36, 66, 0, 0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+    return SafeArea(
+      child: ClipRRect(
+        borderRadius: BorderRadius.horizontal(right: Radius.circular(30.0)),
+        child: Drawer(
+          child: ListView(
+            primary: false,
             children: [
-              DrawerHeader(
-                padding: EdgeInsets.zero,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center ,
-                  children: [
-                    CircleAvatar(
-                      radius: 25.0,
-                      backgroundImage: AssetImage('images/user.png'),
-                    ),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    Text(
-                      'Username',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w700,
+              Padding(
+                padding: const EdgeInsets.only(top: 30.0),
+                child: DrawerHeader(
+                  margin: EdgeInsets.zero,
+                  padding: EdgeInsets.only(left:36),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center ,
+                    children: [
+                      CircleAvatar(
+                        radius: 25.0,
+                        backgroundImage: AssetImage('images/user.png'),
                       ),
-                    ),
-                  ],
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Text(
+                        'Username',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+
+                    ],
+                  ),
                 ),
+              ),
+              SizedBox(
+                height: 35,
               ),
               DrawerTile(icon:Icons.person_rounded,  title:'My Profile'),
               DrawerTile(icon:Icons.calendar_today_outlined,  title:'Schedule'),
@@ -65,7 +72,7 @@ class DrawerTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      contentPadding: EdgeInsets.zero,
+      contentPadding: EdgeInsets.fromLTRB(36, 0, 0, 0),
       horizontalTitleGap: 0,
       leading: Icon(icon),
       title: Text(
