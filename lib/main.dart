327f8575628b589f69f11d9199861bbfbdc58b14
UdithaIshan <uditha.ishan34@gmail.com>
2021-01-10 06:51:19 +0530

    Sizer package added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer_util.dart';
 import 'screen_selector.dart';
 
 void main() => runApp(AirClub());
@@ -6,14 +7,22 @@ void main() => runApp(AirClub());
 class AirClub extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.white,
-        scaffoldBackgroundColor: Colors.white,
-        fontFamily: 'Open Sans'
-      ),
-      home: ScreenSelector(),
-    );
+    return LayoutBuilder(builder: (context, constraints) {
+      return OrientationBuilder(
+        builder: (context, orientation) {
+          SizerUtil().init(constraints, orientation);
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+                primaryColor: Colors.white,
+                scaffoldBackgroundColor: Colors.white,
+                fontFamily: 'Open Sans'
+            ),
+            home: ScreenSelector(),
+          );
+        },
+      );
+    });
+
   }
 }
