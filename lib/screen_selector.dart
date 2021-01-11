0b1ef94c782881974a2c41a8f12b52c6ae09f98f
milanmalshika <milanmalshika22@gmail.com>
2021-01-11 08:50:36 +0530

    Updated search page

--- lib/screen_selector.dart
+++ lib/screen_selector.dart
@@ -45,11 +45,31 @@ class _ScreenSelectorState extends State<ScreenSelector> {
         elevation: 0,
         actions: [
           IconButton(icon: Icon(Icons.search_rounded), onPressed: () {
-            Navigator.of(context).push(MaterialPageRoute(
-                builder: (_){
-                  return SearchScreen();
+            Navigator.of(context).push(
+              PageRouteBuilder(
+                pageBuilder: (context, animation, secondaryAnimation) => SearchScreen(),
+                transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                  return SlideTransition(
+                      position: Tween<Offset>(
+                        begin: const Offset(1.0, 0.0),
+                        end: Offset.zero,
+                      ).animate(animation),
+                    child: SlideTransition(
+                      position: Tween<Offset>(
+                        begin: Offset.zero,
+                        end: const Offset(1.0, 0.0),
+                      ).animate(secondaryAnimation),
+                      child: child,
+                    ),
+                  );
                 }
-            ));
+              )
+//                MaterialPageRoute(
+//                  builder: (_){
+//                    return SearchScreen();
+//                  }
+//                )
+            );
           }),
         ],
       ),
