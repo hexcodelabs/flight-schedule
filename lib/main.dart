30ce1228e4d6410ec486a6234c2da70a221e6241
Ravindu-Ranaweera <inforanaweera@gmail.com>
2021-01-14 01:18:47 +0530

    splashes screen added

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'screen_selector.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,17 +31,48 @@ class AirClub extends StatelessWidget {
             scaffoldBackgroundColor: Colors.white,
             fontFamily: 'Open Sans'
         ),
-
-        initialRoute: SignIn.id,
+        // initialRoute: SignIn.id,
         routes: {
           SignIn.id : (context) => SignIn(),
           SignUp.id : (context) => SignUp(),
           ScreenSelector.id : (context) => ScreenSelector(),
         },
 
-        // home: ScreenSelector(),
+
+
+        home: new MyApp(),
+
       ),
     );
 
+  }
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+        seconds: 15,
+        navigateAfterSeconds: new AfterSplash(),
+        image: Image.asset('images/logo.png'),
+        backgroundColor: Colors.blue[900],
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 150.0,
+        onClick: () => print("Flutter Egypt"),
+        loaderColor: Colors.blue[900]);
+  }
+}
+
+class AfterSplash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: SignIn(),
+
+    );
   }
 }
